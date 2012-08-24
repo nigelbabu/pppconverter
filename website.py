cd6359bb3da3f13b9232b103779e40a1596169c5
Nigel Babu <nigel@hasgeek.com>
2012-08-24 12:35:18 +0530

    Ignore more things

--- website.py
+++ website.py
@@ -3,8 +3,10 @@ import csv
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form, TextField, DecimalField, SelectField, Required
+from baseframe import baseframe, baseframe_js, baseframe_css
 
 
+# Create an app and configure it
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('settings.py')
 app.config.from_pyfile('local.py', silent=True)
@@ -25,23 +27,38 @@ class Country(db.Model):
 
 # Form
 class SalaryForm(Form):
-    from_country = SelectField(u'Country of Origin', coerce=unicode)
-    salary = DecimalField(u'Salary in local currency', validators=[Required()])
-    to_country = SelectField(u'Destination Country', coerce=unicode)
+    from_country = SelectField(u'Where are you from?', coerce=int)
+    salary = DecimalField(u'Salary in your local currency', validators=[Required()])
+    to_country = SelectField(u'Where are you headed to?', coerce=int)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = SalaryForm()
-    form.from_country.choices = ((country.country_code, country.country_name) for country in Country.query.all())
-    form.to_country.choices = ((country.country_code, country.country_name) for country in Country.query.all())
-    currency_value = None
+    currency_value=None
+    form.from_country.choices = [(country.id, country.country_name) for country in Country.query.order_by('country_name').all()]
+    form.to_country.choices = [(country.id, country.country_name) for country in Country.query.order_by('country_name').all()]
     if form.validate_on_submit():
-        from_country = Country.query.filter_by(country_code=form.from_country.data).first()
-        to_country = Country.query.filter_by(country_code=form.to_country.data).first()
-        if from_country or to_country is not None:
-            currency_value = (float(form.salary.data) / float(from_country.ppp)) * to_country.ppp
-    return render_template('index.html', form=form, currency_value=currency_value)
+        fromcountry = Country.query.get(form.from_country.data)
+        tocountry = Country.query.get(form.to_country.data)
+        if fromcountry or tocountry is not None:
+            currency_value = (float(form.salary.data) / float(fromcountry.ppp)) * tocountry.ppp
+    return render_template('index.html', form=form, currency_value=currency_value, home=True)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', about=True)
+
+
+@app.route('/data')
+def data():
+    return render_template('data.html', data=True)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', contact=True)
 
 
 if __name__ == '__main__':
