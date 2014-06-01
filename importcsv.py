a7dc24969b1747f72eab086bf96c829c043b549d
nigelb <nigelbabu@gmail.com>
2014-06-01 11:36:51 +0300

    Use argparse in importcsv

--- importcsv.py
+++ importcsv.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 from website import Country, db
 import csv
+import argparse
 
-ppp_data = csv.reader(open('data.csv'))
-for line in ppp_data:
-    country = Country(name=line[0], code3=line[1], year=line[2], ppp=line[3])
-    db.session.add(country)
-    db.session.commit()
+def main(file_name):
+    with open(file_name) as ppp_data:
+        for line in ppp_data:
+            country = Country(name=line[0], code3=line[1],
+                              year=line[2], ppp=line[3])
+            db.session.add(country)
+            db.session.commit()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Import CSV into the database'
+                                     )
+    parser.add_argument('-f', '--file', required=True, help='Path to the CSV '
+                        'file')
+    args = parser.parse_args()
+    main(args.file)
