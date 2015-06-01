d79a0f908615545bc25d1401a5105b4e3772a2ab
nigelb <nigelbabu@gmail.com>
2015-06-01 21:38:24 +0530

    Cleaned up JS and template a bit
    Former-commit-id: 769e10d073cec69747eef1b439df9624afe4876d

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,11 @@
 $('form select').chosen();
 $('form').submit(function (event) {
   $.getJSON('/json', function(data) {
-    var from_country = Number($('#from_country').attr('value'));
-    var to_country = Number($('#to_country').attr('value'));
+    var from_country = Number($('#from_country').val());
+    var to_country = Number($('#to_country').val());
     var salary = $('#salary');
-    if (salary.attr('value') === '') {
+    console.log(salary.val());
+    if (salary.val() === '') {
       salary.addClass('error');
       $('#error_salary').removeClass('hidden');
       $('#error_salary').addClass('error');
@@ -12,19 +13,19 @@ $('form').submit(function (event) {
       $('#error_salary').removeClass('error');
       $('#error_salary').addClass('hidden');
       salary.removeClass('error');
-      salary = salary.attr('value');
+      salary = salary.val();
       var from_country_data = [];
       var to_country_data = [];
-      $.each(data['countries'], function(key, country) {
-        if(country['id'] === from_country) {
+      $.each(data.countries, function(key, country) {
+        if(country.id === from_country) {
           from_country_data = country;
-        } else if(country['id'] === to_country) {
+        } else if(country.id === to_country) {
           to_country_data = country;
         }
       });
-      var equivalent_salary = (Number(salary) / from_country_data['ppp']) * to_country_data['ppp'];
-      console.log("In " + to_country_data['name'] + ", you should get " + equivalent_salary + " in local currency.");
-      $('#resultDiv').html("In " + to_country_data['name'] + ", you should get " + Math.round(equivalent_salary * 100)/100 + " in local currency.");
+      var equivalent_salary = (Number(salary) / from_country_data.ppp) * to_country_data.ppp;
+      console.log("In " + to_country_data.name + ", you should get " + equivalent_salary + " in local currency.");
+      $('#resultDiv').html("In " + to_country_data.name + ", you should get " + Math.round(equivalent_salary * 100)/100 + " in local currency.");
       $('#displayResult').removeClass('hidden');
     }
   });
