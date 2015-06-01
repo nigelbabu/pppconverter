3a1ad6b0da88ddfd3b754e83ec47e8f8f6759440
nigelb <nigelbabu@gmail.com>
2015-06-01 20:31:28 +0530

    Remove all the unnecessary static files
    Former-commit-id: 172402c3fdf3366867c310ca3450662ef738cd0c

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,33 @@
-(function($){  
-  $(function(){
-    $(document).foundationMediaQueryViewer();
-    
-    $(document).foundationAlerts();
-    $(document).foundationAccordion();
-    $(document).tooltips();
-    $('input, textarea').placeholder();
-    
-    
-    
-    $(document).foundationButtons();
-    
-    
-    
-    $(document).foundationNavigation();
-    
-    
-    
-    $(document).foundationCustomForms();
-    
-    
-    
-      
-      $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
-      
-    
-    
-    
-    $("#featured").orbit();
-    
-    
-    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+$('form select').chosen();
+$('form').submit(function (event) {
+  $.getJSON('/json', function(data) {
+    var from_country = Number($('#from_country').attr('value'));
+    var to_country = Number($('#to_country').attr('value'));
+    var salary = $('#salary');
+    if (salary.attr('value') === '') {
+      salary.addClass('error');
+      $('#error_salary').removeClass('hidden');
+      $('#error_salary').addClass('error');
+    } else {
+      $('#error_salary').removeClass('error');
+      $('#error_salary').addClass('hidden');
+      salary.removeClass('error');
+      salary = salary.attr('value');
+      var from_country_data = [];
+      var to_country_data = [];
+      $.each(data['countries'], function(key, country) {
+        if(country['id'] === from_country) {
+          from_country_data = country;
+        } else if(country['id'] === to_country) {
+          to_country_data = country;
+        }
+      });
+      var equivalent_salary = (Number(salary) / from_country_data['ppp']) * to_country_data['ppp'];
+      console.log("In " + to_country_data['name'] + ", you should get " + equivalent_salary + " in local currency.");
+      $('#resultDiv').html("In " + to_country_data['name'] + ", you should get " + Math.round(equivalent_salary * 100)/100 + " in local currency.");
+      $('#displayResult').removeClass('hidden');
+    }
   });
-  
-})(jQuery);
+  return false;
+});
+
