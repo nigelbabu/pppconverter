308003fbf0ce7e70771d5e4adb5b9693927e9a5f
Nigel Babu <nigelb@redhat.com>
2017-02-11 13:26:13 +0530

    Make changes for modern times
    Former-commit-id: 78ebd0cb8cb5a915d3549176bc05037ce05d2302

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import csv
 import codecs
 import json
 import requests
-from flask.ext.script import Manager
+from flask_script import Manager
 from website import Country, Config, db, app
 
 manager = Manager(app)
@@ -41,14 +41,7 @@ def parsecsv(file_name = None):
     '''Parse a CSV file from the World Bank'''
 
     def extract_values(data):
-        list_of_years = ['2014', '2013', '2012', '2011', '2010', '2009',
-                '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001',
-                '2000', '1999', '1998', '1997', '1996', '1995', '1994', '1993',
-                '1992', '1991', '1990', '1989', '1988', '1987', '1986', '1985',
-                '1984', '1983', '1982', '1981', '1980', '1979', '1978', '1977',
-                '1976', '1975', '1974', '1973', '1972', '1971', '1970', '1969',
-                '1968', '1967', '1966', '1965', '1964', '1963', '1962', '1961',
-                '1960']
+        list_of_years = ['2015']
 
         for year in list_of_years:
             if data.get(year):
