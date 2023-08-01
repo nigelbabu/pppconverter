611ecef92f17710ffd137c7c0bf54f174ab563b8
Nigel Babu <nigel@nigelb.me>
2023-08-01 15:52:02 +0530

    Oops, fix validation

--- main.go
+++ main.go
@@ -191,7 +191,7 @@ func validateData() error {
 		return fmt.Errorf("validateData - USA: %w", err)
 	}
 	targetAmt := (float64(100) / srcCountry.PPP) * targetCountry.PPP
-	if targetAmt > 0 {
+	if targetAmt < 0 {
 		return fmt.Errorf("100 GBP = %.2fUSD: %w", targetAmt, ErrorBadData)
 	}
 	return nil
